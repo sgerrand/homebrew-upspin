b016fd0a58cbc948075097dad9eeee1fdf429f8d
Sasha Gerrand <upspin.io@sgerrand.com>
2017-12-03 23:15:52 +0000

    :zap: Bump version

--- upspin.rb
+++ upspin.rb
@@ -6,7 +6,8 @@ class Upspin < Formula
 
   devel do
     url "https://upspin.io/dl/upspin.darwin_amd64.tar.gz"
-    sha256 "d552d88ddef99b250f2ab2217a1556782dbdb2f4cf5a4b776737f12ea18426fd"
+    sha256 "a9e0bdf6058440678dcf6874a6a9a8007cbc5773a087e44eccb9ad566ca5cab0"
+    version "a9e0bdf6058440678dcf6874a6a9a8007cbc5773a087e44eccb9ad566ca5cab0"
   end
 
   def install
@@ -17,6 +18,6 @@ class Upspin < Formula
     # Upspin requires a user to be configured and network availablity.
     # Without that, this is a basic check to verify the command runs.
     assert_match %r{^Usage of upspin}, shell_output("#{bin}/upspin -help 2>&1", 2)
-    assert_match %r{^Build time: Oct 27 21:33:19 UTC\nGit hash:   24e3a932a78985a544d207a704701ad8f7821388\n}, shell_output("#{bin}/upspin -version 2>&1", 2)
+    assert_match %r{^Build time: Dec  1 19:23:12 2017 UTC\nGit hash:   3d915eafc9821e06a0d90b9f803d5426e3d4c9d4\n}, shell_output("#{bin}/upspin -version 2>&1", 2)
   end
 end
